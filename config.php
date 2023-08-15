4f7ee1b391e10a103756a00e730f38efcb6a9be6
Nathan Campos <nathan@innoveworkshop.com>
2023-08-15 10:46:02 +0100

    Moved the configuration file to the root of the project
    We'll only have a single configuration file so it doesn't make sense for
    it to live alone in its own folder.

--- config.php
+++ config.php
@@ -6,16 +6,16 @@
  * @author Nathan Campos <nathan@innoveworkshop.com>
  */
 
- /**
-  * Defines a configuration constant to be used in the application from an
-  * environment variable of the same name. If the environment variable was not
-  * set it uses a default value instead.
-  *
-  * @param string $name    Name of the environment variable.
-  * @param any    $default Default value if the environment variable wasn't set.
-  * @param string $varname Constant name to be used if it should be different
-  *                        than the environment variable name.
-  */
+/**
+ * Defines a configuration constant to be used in the application from an
+ * environment variable of the same name. If the environment variable was not
+ * set it uses a default value instead.
+ *
+ * @param string $name    Name of the environment variable.
+ * @param any    $default Default value if the environment variable wasn't set.
+ * @param string $varname Constant name to be used if it should be different
+ *                        than the environment variable name.
+ */
 function env_define_const($name, $default, $varname = null) {
 	// If a constant name wasn't passed use the environment variable name.
 	if (is_null($varname))
@@ -31,4 +31,3 @@ env_define_const('LINKDEPOT_APP_NAME', 'Link Depot', 'APP_NAME');
 // Site definitions.
 env_define_const('LINKDEPOT_SITE_PATH', 'linkdepot', 'SITE_PATH');
 define('SITE_URL', '/' . SITE_PATH);
-
