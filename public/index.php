32fca8958a0798af9172f3ccdd1d6f43dc0596ee
Nathan Campos <nathan@innoveworkshop.com>
2023-08-16 12:17:08 +0100

    Designed the visual aspect of how a link list will look like
    Links will be placed inside grey boxes with the favicon on the left, the
    title and the link itself on the right. A very simple and elegant design
    that works on every platform.

--- public/index.php
+++ public/index.php
@@ -3,4 +3,39 @@
 <p>A minimalist way to store, manage, and work with your bookmarks and random
 	URLs from the internet.</p>
 
+<div class="link-shelf">
+	<h3 class="shelf-title">Desktop</h3>
+	<table class="link-box">
+		<tr>
+			<td class="col-icon">
+				<img class="favicon" src="https://innoveworkshop.com/favicon.ico" />
+			</td>
+			<td class="col-desc">
+				<div class="link-title">Innove Workshop</div>
+				<a class="link-url" href="https://innoveworkshop.com/">https://innoveworkshop.com/</a>
+			</td>
+		</tr>
+		<tr class="spacer"></tr>
+		<tr>
+			<td class="col-icon">
+				<img class="favicon" src="https://github.com/favicon.ico" />
+			</td>
+			<td class="col-desc">
+				<div class="link-title">nathanpc/linkdepot: A minimalist way to store, manage, and work with your bookmarks and random URLs from the internet</div>
+				<a class="link-url" href="https://github.com/nathanpc/linkdepot">https://github.com/nathanpc/linkdepot</a>
+			</td>
+		</tr>
+		<tr class="spacer"></tr>
+		<tr>
+			<td class="col-icon">
+				<img class="favicon" src="https://developer.mozilla.org/favicon.ico" />
+			</td>
+			<td class="col-desc">
+				<div class="link-title">&lt;table&gt;: The Table element - HTML: HyperText Markup Language | MDN</div>
+				<a class="link-url" href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/table">https://developer.mozilla.org/en-US/docs/Web/HTML/Element/table</a>
+			</td>
+		</tr>
+	</table>
+</div>
+
 <?php require(__DIR__ . "/../templates/footer.php"); ?>
