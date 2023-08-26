9658da0fdb62eea4b5d8b2e9714a2fef49d99cbe
Nathan Campos <nathan@innoveworkshop.com>
2023-08-26 19:09:58 +0100

    Cleaned up the index page
    Removed the test elements for how the link shelf was supposed to look
    like since we no longer need it.

--- public/index.php
+++ public/index.php
@@ -7,28 +7,4 @@
 	<?= $shelf->as_html() ?>
 <?php } ?>
 
-<div class="link-shelf">
-	<div class="shelf-header">
-		<h3>Desktop</h3>
-		<span class="shelf-actions">
-			<a href="#">store</a> ‧
-			<a href="#">manage</a> ‧
-			<a class="action-delete" href="#">delete</a>
-		</span>
-	</div>
-
-	<table class="link-box">
-		<?= link_box_row("Innove Workshop",
-			"https://innoveworkshop.com/",
-			"https://innoveworkshop.com/favicon.ico") ?>
-		<?= link_box_row("nathanpc/linkdepot: A minimalist way to store, manage, and work with your bookmarks and random URLs from the internet",
-			"https://github.com/nathanpc/linkdepot",
-			"https://github.com/favicon.ico") ?>
-		<?= link_box_row("<table>: The Table element - HTML: HyperText Markup Language | MDN",
-			"https://developer.mozilla.org/en-US/docs/Web/HTML/Element/table",
-			"https://developer.mozilla.org/favicon.ico",
-			false, false) ?>
-	</table>
-</div>
-
 <?php require(__DIR__ . "/../templates/footer.php"); ?>
