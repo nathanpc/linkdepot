63384de2b639161ed445218ca1dd722e1f274f93
Nathan Campos <nathan@innoveworkshop.com>
2023-08-26 11:18:33 +0100

    Added a link card to the link added successfully template
    In order to make the "Link added successfully" page a bit more useful,
    and its design a tiny bit more appealing, the finalized link card with
    the editing menu has been added to the page.

--- templates/link/add_success.php
+++ templates/link/add_success.php
@@ -1,7 +1,11 @@
 <p>
-	Link <a href="<?= $link->url() ?>"><?= $link->title() ?></a><sup>
-	(<a href="<?= href("/link.php?action=edit&id={$link->id()}") ?>">edit</a>)
-	</sup> successfully added to the
+	Link successfully added to the
 	<a href="<?= href("/shelf.php?action=manage&id={$link->shelf()->id()}") ?>">
 	<?= $link->shelf()->title() ?></a> shelf.
 </p>
+
+<div class="link-shelf">
+	<table class="link-box">
+		<?= $link->as_html(true) ?>
+	</table>
+</div>
