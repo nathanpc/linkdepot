0c45725b53a0fbd6c67138f12b538f2505cc1ccf
Nathan Campos <nathan@innoveworkshop.com>
2023-08-31 18:54:24 +0100

    Added a very simple about page
    Started the work on the about page, which will be very important in the
    future.

---
+++ public/about.php
@@ -0,0 +1,6 @@
+<?php require(__DIR__ . "/../templates/head.php"); ?>
+
+<p>A minimalist way to store, manage, and work with your bookmarks and random
+	URLs from the internet.</p>
+
+<?php require(__DIR__ . "/../templates/footer.php"); ?>
